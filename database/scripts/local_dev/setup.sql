266d99a1eaac41dc04a859aec73cb52b1e68c5eb
Fergus MacConnell WLRS:EX <f.macconnell@gmail.com>
2024-11-25 09:53:24 -0800

    Resolve test related issues when running in docker.

--- database/scripts/local_dev/setup.sql
+++ database/scripts/local_dev/setup.sql
@@ -13,7 +13,7 @@ create extension "uuid-ossp";
 
 -- create user "PG_PRIMARY_USER" with REPLICATION  PASSWORD 'PG_PRIMARY_PASSWORD';
 -- create user "PG_USER" with password 'PG_PASSWORD' SUPERUSER;
--- create user ftw_reader with password 'PG_PASSWORD';
+create user ftw_reader with password 'PG_PASSWORD';
 
 -- create table primarytable (key varchar(20), value varchar(20));
 -- grant all on primarytable to "PG_PRIMARY_USER";
@@ -53,6 +53,6 @@ create extension "uuid-ossp";
 
 -- grant all on "PG_USER".testtable to "PG_PRIMARY_USER";
 
--- create schema postgis_ftw;
--- grant usage on schema postgis_ftw to ftw_reader;
--- ALTER DEFAULT PRIVILEGES IN SCHEMA postgis_ftw GRANT SELECT ON TABLES TO ftw_reader;
+create schema postgis_ftw;
+grant usage on schema postgis_ftw to ftw_reader;
+ALTER DEFAULT PRIVILEGES IN SCHEMA postgis_ftw GRANT SELECT ON TABLES TO ftw_reader;
